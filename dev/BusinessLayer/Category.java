7f13ce9d6d29f7230329027c9ab2ed977c035c17
rotemchn <63972323+rotemchn@users.noreply.github.com>
2021-04-06 12:52:35 +0300

    Classes

---
+++ dev/BusinessLayer/Category.java
@@ -0,0 +1,15 @@
+package BusinessLayer;
+
+import java.util.Date;
+import java.util.List;
+
+public class Category {
+
+    private String name;
+    private List<Category> subCategories;
+    private List<Product> products;
+    private int discount;
+    private Date discountDate;
+
+
+}
