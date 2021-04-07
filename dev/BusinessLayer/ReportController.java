fea47df2c17cc7c591ad807052ec140ef998e83c
hilaklein <63915907+hilaklein@users.noreply.github.com>
2021-04-07 19:07:30 +0300

    ReportController update;

--- dev/BusinessLayer/ReportController.java
+++ dev/BusinessLayer/ReportController.java
@@ -4,23 +4,58 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ReportController {
-    List<Report> reports;
+    List<DefectiveReport> defReports;
+    List<StockReport> stockReports;
 
 
     public ReportController() {
-        this.reports = new LinkedList<>();
+        this.defReports = new LinkedList<>();
+        this.stockReports = new LinkedList<>();
     }
 
-    public void addStockReport (List<Category> categories){
+    public DefectiveReport getDefReport(int id) {
+        for (DefectiveReport dRep : defReports) {
+            if (dRep.getID() == id)
+                return dRep;
+        }
+        return null;
+    }
+
+    public StockReport getStoReport(int id) {
+        for (StockReport sRep : stockReports) {
+            if (sRep.getID() == id)
+                return sRep;
+        }
+        return null;
+    }
+
+    public void addStockReport(List<Category> categories) {
         StockReport sto = new StockReport();
-        for ( Category c : categories)
+        for (Category c : categories)
             sto.addCategory(c);
     }
 
-    public void addDefReport (List<Product> products) {
+    public void addDefReport(List<Product> products) {
         DefectiveReport def = new DefectiveReport();
         for (Product p : products)
             def.addProd(p);
     }
 
+    public void addCatToStRep(int id, Category category) {
+        StockReport sRep = getStoReport(id);
+        if (sRep != null)
+            sRep.addCategory(category);
+    }
+
+    public String exportReport(int id) {
+        StockReport sRep = getStoReport(id);
+        if (sRep != null) {
+            return sRep.exportReport();
+        } else {
+            DefectiveReport dRep = getDefReport(id);
+            if (dRep != null)
+                return dRep.exportReport();
+        }
+        return "The report is not exist";
+    }
 }
