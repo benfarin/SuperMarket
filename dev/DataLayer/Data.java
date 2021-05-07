b22fcb9a2a25c0c7a6908a0a3a9f624ad823a994
Eylon <eylons@post.bgu.ac.il>
2021-05-07 23:12:40 +0300

    Data Layer start

---
+++ dev/DataLayer/Data.java
@@ -0,0 +1,28 @@
+package DataLayer;
+import java.sql.*;
+
+public class Data {
+
+    private Connection con;
+    public Data(){
+        con = null;
+        connect();
+    }
+
+    public void connect(){
+        try {
+          //  Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection("jdbc:sqlite:dev/SQL_Inventory_Suppliers.db");
+        }
+
+        catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("database successfully created");
+    }
+
+
+
+
+}
--- dev/Main.java
+++ dev/Main.java
@@ -1,9 +1,11 @@
+import DataLayer.Data;
 import PresentationLayer.Inventory.InvService;
 import PresentationLayer.Service;
 
 public class Main {
     public static void main(String[] args){
-     Service s = new Service();
-        s.startMenu();
+   //  Service s = new Service();
+     //   s.startMenu();
+        Data d = new Data();
     }
 }
